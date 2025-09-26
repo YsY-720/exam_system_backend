d27e70d992fc70e3f44390270c407a7c1f084ff4
yusiyuan <1378209949@qq.com>
2025-09-26 18:10:04 +0800

    feat:添加email公共模块；完善注册接口

--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -1,22 +1,60 @@
-import { Inject, Injectable } from '@nestjs/common'
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common'
 import { PrismaService } from '@app/prisma'
-import { Prisma } from '@prisma/client'
+import { RedisService } from '@app/redis'
+import { RegisterUserDto } from './dto/register-user.dto'
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return 'Hello World!'
-  }
-
   @Inject(PrismaService)
   private prismaService: PrismaService
 
-  async create(data: Prisma.UserCreateInput) {
-    return await this.prismaService.user.create({
-      data,
-      select: {
-        id: true,
+  @Inject(RedisService)
+  private redisService: RedisService
+
+  private logger = new Logger()
+
+  async register(data: RegisterUserDto) {
+    const captcha = await this.redisService.get(`captcha_${data.email}`)
+    if (!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST)
+    }
+    if (data.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST)
+    }
+
+    const foundUser = await this.prismaService.user.findUnique({
+      where: {
+        username: data.username,
       },
     })
+
+    if (foundUser) {
+      throw new HttpException('用户已存在', HttpStatus.BAD_REQUEST)
+    }
+
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          username: data.username,
+          password: data.password,
+          email: data.email,
+        },
+        select: {
+          id: true,
+          username: true,
+          email: true,
+          createTime: true,
+        },
+      })
+    } catch (e) {
+      this.logger.error(e, UserService)
+      return null
+    }
   }
 }
