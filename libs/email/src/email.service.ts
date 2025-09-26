af5a4ffb2f3d897b684ec24f0e719ebed9c6e309
yusiyuan <1378209949@qq.com>
2025-09-26 18:30:02 +0800

    feat:配置邮件服务

--- libs/email/src/email.service.ts
+++ libs/email/src/email.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common'
+import { createTransport, Transporter } from 'nodemailer'
 
 @Injectable()
-export class EmailService { }
+export class EmailService {
+  transporter: Transporter
+
+  constructor() {
+    this.transporter = createTransport({
+      host: 'smtp.qq.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: '2285683024@qq.com',
+        pass: 'btlnvjhuwegxdjei',
+      },
+    })
+  }
+
+  async sendEmail({ to, subject, html }) {
+    await this.transporter.sendMail({
+      from: {
+        name: 'exam system',
+        address: '2285683024@qq.com',
+      },
+      to,
+      subject,
+      html,
+    })
+  }
+}
